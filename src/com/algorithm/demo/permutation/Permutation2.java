fbf5fe3387e6acbce2c8c20c023dcb757ec9268a
Trang Truong <sgtt003@AR400384PC.AvivaAP.local>
2016-04-25 18:44:56 +0800

    sum 3

--- src/com/algorithm/demo/permutation/Permutation2.java
+++ src/com/algorithm/demo/permutation/Permutation2.java
@@ -1,6 +1,22 @@
 package com.algorithm.demo.permutation;
 
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+/**
+ * http://www.programcreek.com/2013/02/leetcode-permutations-ii-java/
+ * @author sgtt003
+ *
+ */
 public class Permutation2 {
+	
+	/**
+	 * Use set to maintain uniqueness:
+	 * @param num
+	 * @return
+	 */
 	public static ArrayList<ArrayList<Integer>> permuteUnique(int[] num) {
 		ArrayList<ArrayList<Integer>> returnList = new ArrayList<ArrayList<Integer>>();
 		returnList.add(new ArrayList<Integer>());
@@ -19,5 +35,54 @@ public class Permutation2 {
 		}
 	 
 		return returnList;
+	}
+	
+	/**
+	 * For each number in the array, swap it with every element after it. To
+	 * avoid duplicate, we need to check the existing sequence first.
+	 */
+	public ArrayList<ArrayList<Integer>> permuteUnique2(int[] num) {
+		ArrayList<ArrayList<Integer>> result = new ArrayList<ArrayList<Integer>>();
+		permuteUnique(num, 0, result);
+		return result;
+	}
+	 
+	private void permuteUnique(int[] num, int start, ArrayList<ArrayList<Integer>> result) {
+	 
+		if (start >= num.length ) {
+			ArrayList<Integer> item = convertArrayToList(num);
+			result.add(item);
+		}
+	 
+		for (int j = start; j <= num.length-1; j++) {
+			if (containsDuplicate(num, start, j)) {
+				swap(num, start, j);
+				permuteUnique(num, start + 1, result);
+				swap(num, start, j);
+			}
+		}
+	}
+	 
+	private ArrayList<Integer> convertArrayToList(int[] num) {
+		ArrayList<Integer> item = new ArrayList<Integer>();
+		for (int h = 0; h < num.length; h++) {
+			item.add(num[h]);
+		}
+		return item;
+	}
+	 
+	private boolean containsDuplicate(int[] arr, int start, int end) {
+		for (int i = start; i <= end-1; i++) {
+			if (arr[i] == arr[end]) {
+				return false;
+			}
+		}
+		return true;
+	}
+	 
+	private void swap(int[] a, int i, int j) {
+		int temp = a[i];
+		a[i] = a[j];
+		a[j] = temp;
 	}
 }
